4d87d24b3b4b86fd6fd06e7194d8529ae3182c5c
Ilan Kor <ilankor@meta.com>
2025-03-10 22:49:39 +0200

    Update frontend to use nginx.conf with environment variables

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,17 +1,7 @@
 #!/bin/sh
 
-# Print environment variables
-echo "Environment variables:"
-env | sort
-
-# Replace environment variables in nginx.conf
-echo "Replacing environment variables in nginx.conf..."
-envsubst '${BACKEND_URL} ${PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-
-# Print the generated nginx configuration
-echo "Generated nginx configuration:"
-cat /etc/nginx/conf.d/default.conf
+# Replace environment variables in nginx config
+envsubst '$PORT $BACKEND_URL' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Start nginx
-echo "Starting nginx..."
-exec nginx -g 'daemon off;' 
+nginx -g 'daemon off;' 
